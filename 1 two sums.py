735609ec34806d8c11dac4735888be47f5b5f845
SarvikaN <sarvisakthi66@gmail.com>
2026-04-05 20:21:51 +0530

    1 two sums.py

---
+++ 1 two sums.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m={}
+        for i,num in enumerate(nums):
+            c=target-num
+            if c in m:
+                return[m[c],i]
+            m[num]=i
+        return []
