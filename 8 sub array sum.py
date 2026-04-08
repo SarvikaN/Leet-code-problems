327df7ba18dbb341a10a28553646bf9db3ce4bd6
SarvikaN <sarvisakthi66@gmail.com>
2026-04-08 23:36:32 +0530

    8 sub array sum.py

---
+++ 8 sub array sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        d = {0: 1}
+        s = 0
+        count = 0
+        for num in nums:
+            s += num
+            if (s - k) in d:
+                count += d[s - k]
+            d[s] = d.get(s, 0) + 1
+        return count
