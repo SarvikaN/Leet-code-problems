06fa0227f61bf20d58a547dcc42eb5ee9afefae5
SarvikaN <sarvisakthi66@gmail.com>
2026-04-29 22:01:39 +0530

    14 course schedule.py

---
+++ 14 course schedule.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        graph = {i: [] for i in range(numCourses)}
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        count = 0
+        while q:
+            course = q.popleft()
+            count += 1
+            for nei in graph[course]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return count == numCourses
